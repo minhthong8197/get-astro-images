8eb2ddf2cf1d3e35c070c62c1b43d5a3e2751fcd
minhthong8197 <minhthong8197@gmail.com>
2022-04-03 21:29:10 +0700

    feat: update getLink

--- getLink.js
+++ getLink.js
@@ -11,9 +11,12 @@ listEle = document.querySelectorAll(".New_Object");
  * @type string[]
  */
 listLink = Array.from(listEle).reduce((output, eachEle) => {
-  const wheatherNumber = Number(
-    eachEle.childNodes[eachEle.childNodes.length - 1].innerText
-  );
+  const wheatherText =
+    eachEle.childNodes[eachEle.childNodes.length - 1].innerText;
+  // check case skip
+  if (wheatherText[0] === "-") return output;
+  const wheatherNumber = Number(wheatherText.match(/^\d+/));
+
   if (wheatherNumber >= 97)
     output.push(eachEle.childNodes[3].childNodes[0].href);
   return output;
